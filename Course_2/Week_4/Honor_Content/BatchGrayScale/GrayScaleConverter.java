1b5a20dcc8ebe84c398c51806f4639c8089323b4
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-09-24 04:43:33 +0100

    Created Methods in BatchGrayScale class

--- Course_2/Week_4/Honor_Content/BatchGrayScale/GrayScaleConverter.java
+++ Course_2/Week_4/Honor_Content/BatchGrayScale/GrayScaleConverter.java
@@ -5,6 +5,47 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class GrayScaleConverter {
 
+import edu.duke.*;
+import java.io.*;
+
+public class GrayScaleConverter {
+    // Get an Image and convert to grayscale
+    public ImageResource makeGray(ImageResource inImage) {
+        // Create a blank image of same size
+        ImageResource outImage = new ImageResource(inImage.getWidth(),
+                                                    inImage.getHeight());
+        // Iterate through pixels in the image
+        for (Pixel pixel : outImage.pixels()) {
+            // get corresponding pixel from inImage
+            Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
+            // get the average of the RGB values of the pixel
+            int average = (inPixel.getRed() + inPixel.getBlue() + 
+                            inPixel.getGreen()) / 3;
+            // set the RGB values of pixel to the average
+            pixel.setRed(average);
+            pixel.setBlue(average);
+            pixel.setGreen(average);
+        }
+        return outImage;
+    }
+    
+    // Method to select images and convert to grayscale
+    public void selectAndConvert() {
+        // create directory
+        DirectoryResource dr = new DirectoryResource();
+        // iterate through resource to get files
+        for (File f : dr.selectedFiles()) {
+            // get Image resource from current file
+            ImageResource ir = new ImageResource(f);
+            ImageResource gray = makeGray(ir);
+            gray.draw();
+        }
+    }
+    
+    public void testGray() {
+        ImageResource ir = new ImageResource();
+        ImageResource gray = makeGray(ir);
+        gray.draw();
+    }
 }
