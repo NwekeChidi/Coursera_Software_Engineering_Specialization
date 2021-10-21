b1cdbf9325f45e0d2249cd3d90844997905f38d9
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-10-21 18:21:53 +0100

    Final Commit on CountOccurrence class

---
+++ Course_3/Week_1/BreakingCeasarCipher/CountOccurrence.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of CountOccurrence here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+
+public class CountOccurrence {
+    public int[] countOccurrence(String message){
+        String alph = "abcdefghijklmnopqrstuvwxyz";
+        int[] counts = new int[26];
+        for(int i=0; i<message.length(); i++){
+            char ch = Character.toLowerCase(message.charAt(i));
+            int idx = alph.indexOf(ch);
+            if (idx != -1){
+                counts[idx] += 1;
+            }
+        }
+        return counts;
+    }
+    
+    public int maxIndex(int[] vals){
+        int maxIdx = 0;
+        for(int i=0; i < vals.length; i++){
+            if (vals[i] > vals[maxIdx]){
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+}
