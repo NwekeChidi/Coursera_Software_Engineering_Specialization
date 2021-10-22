a4fa9bfae019101bd80199f91f23065566399fc9
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-10-23 00:09:05 +0100

    Created Object Oriented CaesarCipher Class with required methods

--- Course_3/Week_1/OOCeasarCipher/CeasarCipher.java
+++ Course_3/Week_1/OOCeasarCipher/CeasarCipher.java
@@ -9,21 +9,33 @@
 import edu.duke.*;
 import java.io.*;
 public class CeasarCipher {
-    public String encrypt(String input, int key){
+    private String alphabet;
+    private String shiftedAlphabet;
+    private int decryptKey;
+    // Write constructor
+    public CeasarCipher(int key){
+        // Write down the alphabet
+        alphabet = "abcdefghijklmnopqrstuvwxyz";
+        // Compute shifted alphabet
+        shiftedAlphabet = alphabet.substring(key) +
+                          alphabet.substring(0, key);
+        // get key
+        decryptKey = 26-key;
+    }
+    
+    public String encrypt(String input){
         // Make a StringBuilder with message (encrypted)
         StringBuilder encrypted = new StringBuilder(input);
         // Count from 0 < length of encrypted
         for (int i=0; i< encrypted.length(); i++){
-            // Write down the alphabet
-            String alphabet = "abcdefghijklmnopqrstuvwxyz";
-            // Compute shifted alphabet
-            String shiftedAlphabet = alphabet.substring(key) +
-                            alphabet.substring(0, key);
+            // keep alphabet and shifted alphabet in lowercase
+            alphabet = alphabet.toLowerCase();
+            shiftedAlphabet = shiftedAlphabet.toLowerCase();
             // Look at the ith character of encrypted
             char currChar = encrypted.charAt(i);
             // Check the case of the current character
             if (Character.isUpperCase(currChar)){
-                // if true, convert alphabet and shifted alphabet toLowerCase
+                // if true, convert alphabet and shifted alphabet toUpperCase
                 alphabet = alphabet.toUpperCase();
                 shiftedAlphabet = shiftedAlphabet.toUpperCase();
             }
@@ -41,59 +53,8 @@ public class CeasarCipher {
         return encrypted.toString();
     }
     
-    public String encryptTwoKeys(String input, int key1, int key2){
-        // Make a StringBuilder with message (encrypted)
-        StringBuilder encrypted = new StringBuilder(input);
-        // Count from 0 < length of encrypted
-        for (int i=0; i< encrypted.length(); i++){
-            // Write down the alphabet
-            String alphabet = "abcdefghijklmnopqrstuvwxyz";
-            // Compute shifted alphabet
-            String shiftedAlphabet1 = alphabet.substring(key1) +
-                            alphabet.substring(0, key1);
-            String shiftedAlphabet2 = alphabet.substring(key2) +
-                            alphabet.substring(0, key2);
-            // Look at the ith character of encrypted
-            char currChar = encrypted.charAt(i);
-            // Check the case of the current character
-            if (Character.isUpperCase(currChar)){
-                // if true, convert alphabet and shifted alphabet toLowerCase
-                alphabet = alphabet.toUpperCase();
-                shiftedAlphabet1 = shiftedAlphabet1.toUpperCase();
-                shiftedAlphabet2 = shiftedAlphabet2.toUpperCase();
-            }
-            // Find the index of currChar in the alphabet
-            int idx = alphabet.indexOf(currChar);
-            // If currChar is in the alphabet
-            if (idx != -1 && i%2==0){
-                // Get the idxth character of shiftedAlphabet
-                char newChar = shiftedAlphabet1.charAt(idx);
-                // Replace the ith character of enctripted
-                encrypted.setCharAt(i, newChar);
-            }
-            if (idx != -1 && i%2!=0){
-                // Get the idxth character of shiftedAlphabet
-                char newChar = shiftedAlphabet2.charAt(idx);
-                // Replace the ith character of enctripted
-                encrypted.setCharAt(i, newChar);
-            }
-        }
-        // Return answer
-        return encrypted.toString();
-    }
-    
-    public void testCeaser(int key, int key1, int key2){
-        FileResource fr =  new FileResource();
-        String message = fr.asString();
-        String encrypted = encrypt(message,key);
-        System.out.println("Testing encrypt method .....");
-        System.out.println("Encrypted: \n"+encrypted);
-        String decrypted = encrypt(encrypted, 26-key);
-        System.out.println("Decrypted: \n"+decrypted);
-        System.out.println("\nTesting encryptTwoKeys method .....");
-        encrypted = encryptTwoKeys(message, key1, key2);
-        System.out.println("Encrypted: \n"+encrypted);
-        decrypted = encryptTwoKeys(encrypted, 26-key1, 26-key2);
-        System.out.println("Decrypted: \n"+decrypted);
+    public String decrypt(String input){
+        CeasarCipher cc = new CeasarCipher(decryptKey);
+        return cc.encrypt(input);
     }
 }
