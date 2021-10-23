10c64911de248e8e2d5e3d96088f637ab2f408cb
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-10-23 08:13:52 +0100

    Final Commit

--- Course_3/Week_1/OOCeasarCipher/TestSimple.java
+++ Course_3/Week_1/OOCeasarCipher/TestSimple.java
@@ -10,6 +10,5 @@ public class TestSimple{
      public void print() {  	
           Simple item = new Simple(3, "blue");     	
           System.out.println(item);
-          System.out.println(item.mystery(5, "ho"));
      }      
 }
