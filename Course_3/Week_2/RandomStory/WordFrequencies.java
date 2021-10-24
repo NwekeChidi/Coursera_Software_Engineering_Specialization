5023805f3af4d64f0bed0ca741863d31e8607490
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-10-24 09:20:43 +0100

    Created WordFrequencies class

--- Course_3/Week_2/RandomStory/WordFrequencies.java
+++ Course_3/Week_2/RandomStory/WordFrequencies.java
@@ -5,6 +5,39 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class WordFrequencies {
+import edu.duke.*;
+import java.util.ArrayList;
 
+public class WordFrequencies {
+    private ArrayList<String> myWords;
+    private ArrayList<Integer> myFreqs;
+    
+    public WordFrequencies() {
+        myWords = new ArrayList<String>();
+        myFreqs = new ArrayList<Integer>();
+    }
+    
+    public void findUnique(){
+        FileResource resource = new FileResource();
+        for (String s : resource.words()){
+            s = s.toLowerCase();
+            int index = myWords.indexOf(s);
+            if (index != -1){
+                int value = myFreqs.get(index);
+                myFreqs.set(index, value+1);;
+            }
+            else {
+                myWords.add(s);
+                myFreqs.add(1);
+            }
+        }
+    }
+    
+    public void tester(){
+        findUnique();
+        System.out.println("# unique words: "+myWords.size());
+        for (int i=0; i<myWords.size(); i++){
+            System.out.println(myFreqs.get(i)+"\t"+myWords.get(i));
+        }
+    }
 }
