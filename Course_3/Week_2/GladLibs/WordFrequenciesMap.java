a7754884de8bc268b447f43633de01f7e3061b20
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-10-26 00:53:44 +0100

    Added method and test files

--- Course_3/Week_2/GladLibs/WordFrequenciesMap.java
+++ Course_3/Week_2/GladLibs/WordFrequenciesMap.java
@@ -5,6 +5,28 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import edu.duke.*;
+import java.util.*;
 public class WordFrequenciesMap {
-
+    
+    public void countWords(){
+        FileResource fr = new FileResource();
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        //int total = 0;
+        for (String w : fr.words()){
+            w = w.toLowerCase();
+            if (map.keySet().contains(w)){
+                map.put(w,map.get(w)+1);
+            } else {
+                map.put(w,1);
+            }
+        }
+        System.out.println("total words : "+map.size());
+        for (String w : map.keySet()){
+            int occurrences = map.get(w);
+            if (occurrences > 500){
+                System.out.println(occurrences+"\t"+w);
+            }
+        }
+    }
 }
