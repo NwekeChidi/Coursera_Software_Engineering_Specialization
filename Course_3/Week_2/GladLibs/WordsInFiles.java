828d26203b7869a45e62edc30ebf839e08ac2d6b
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-10-26 12:39:06 +0100

    Completed and debugged methods in class

--- Course_3/Week_2/GladLibs/WordsInFiles.java
+++ Course_3/Week_2/GladLibs/WordsInFiles.java
@@ -32,5 +32,50 @@ public class WordsInFiles {
             }
         }
     }
-
+    
+    private int maxNumber(){
+        int maxNum = 0;
+        for (String key : wordMap.keySet()){
+            if (wordMap.get(key).size() > maxNum){
+                maxNum = wordMap.get(key).size();
+            }
+        }
+        return maxNum;
+    }
+    
+    private ArrayList<String> wordsInNumFiles(int number){
+        ArrayList<String> wordList = new ArrayList<String>();
+        for (String key : wordMap.keySet()){
+            if (wordMap.get(key).size() == number){
+                wordList.add(key);
+            }
+        }
+        return wordList;
+    }
+    
+    private void printFilesIn(String word){
+        for(int i=0; i<wordMap.get(word).size(); i++){
+            System.out.println(wordMap.get(word).get(i));
+        }
+    }
+    
+    public void buildWordFileMap(){
+        wordMap.clear();
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()){
+            addWordsFromFile(f);
+        }
+    }
+    
+    public void tester(){
+        System.out.println("\nRunning WordsInFiles class ......\n\n");
+        buildWordFileMap();
+        System.out.println("Max number of files any word is in: "+maxNumber());
+        System.out.println("Words appearing in "+2+" files: "+wordsInNumFiles(2));
+        for (String key : wordMap.keySet()){
+            System.out.println("\n"+key+":");
+            printFilesIn(key);
+        }
+        System.out.println(wordMap);
+    }
 }
