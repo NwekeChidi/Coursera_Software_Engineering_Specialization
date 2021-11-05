44fdb1725718fe3d1d6e0b6bdd975d10b3a16454
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-11-05 14:55:29 +0100

    Added method 'breakForLanguage'

--- Course_3/Week_4/VigenereProgram/TesterKnown.java
+++ Course_3/Week_4/VigenereProgram/TesterKnown.java
@@ -6,6 +6,8 @@
  * @version (a version number or a date)
  */
 import edu.duke.*;
+import java.util.*;
+
 public class TesterKnown {
     VigenereBreaker vb = new VigenereBreaker();
     
@@ -14,9 +16,15 @@ public class TesterKnown {
         String message = fr.asString();
         int[] itr = {0, 1, 2, 3};
         int totalSlices = 4;
-        for (int whichSlice: itr){
-            System.out.println("\n On whichSlice = "+whichSlice+", resulting string is: \n"+
-                    vb.sliceString(message, whichSlice, totalSlices));
-        }
+        //for (int whichSlice: itr){
+        //    System.out.println("\n On whichSlice = "+whichSlice+", resulting string is: \n"+
+        //            vb.sliceString(message, whichSlice, totalSlices));
+        //}
+    }
+    
+    public void testReadDictionary(){
+        FileResource fr = new FileResource();
+        HashSet<String> dictSet = vb.readDictionary(fr);
+        System.out.println(dictSet);
     }
 }
--- Course_3/Week_4/VigenereProgram/VigenereBreaker.java
+++ Course_3/Week_4/VigenereProgram/VigenereBreaker.java
@@ -14,7 +14,48 @@ public class VigenereBreaker {
     public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
         int[] key = new int[klength];
         //WRITE YOUR CODE HERE
+        CaesarCracker ccr = new CaesarCracker(mostCommon);
+        for(int i=0; i<key.length; i++){
+            String currEncrypt = sliceString(encrypted, i, klength);
+            int currKey = ccr.getKey(currEncrypt);
+            key[i] = currKey;
+        }
         return key;
+    }
+    
+    public HashSet<String> readDictionary(FileResource fr){
+        HashSet<String> set = new HashSet();
+        for (String line : fr.lines()){
+            set.add(line.toLowerCase());
+        }
+        return set;
+    }
+    
+    public int countWords(String message, HashSet<String> dictionary){
+        String[] splitM = message.split("\\W+");
+        int realWords = 0;
+        for (int i=0; i<splitM.length; i++){
+            if(dictionary.contains(splitM[i].toLowerCase())){
+                realWords ++;
+            }
+        }
+        return realWords;
+    }
+    
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary){
+        int realWords = 0;
+        char mostCommon = 'e';
+        String finalDecrypt = "";
+        for(int i=0; i<encrypted.length(); i++){
+            int[] keyLength = tryKeyLength(encrypted, i, mostCommon);
+            VigenereCipher vc = new VigenereCipher(keyLength);
+            String decrypted = vc.decrypt(encrypted);
+            int currRealWords = countWords(decrypted, dictionary);
+            if (currRealWords > realWords){
+                finalDecrypt = decrypted;
+            }
+        }
+        return finalDecrypt;
     }
 
     public void breakVigenere () {
