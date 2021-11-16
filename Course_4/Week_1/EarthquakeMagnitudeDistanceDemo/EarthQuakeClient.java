78b25430e4ce87a4e35859b3c9b23aac1fecfc6c
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-11-16 05:29:42 +0100

    Added method 'quakesOfDistance'

--- Course_4/Week_1/EarthquakeMagnitudeDistanceDemo/EarthQuakeClient.java
+++ Course_4/Week_1/EarthquakeMagnitudeDistanceDemo/EarthQuakeClient.java
@@ -29,6 +29,7 @@ public class EarthQuakeClient {
     public ArrayList<QuakeEntry> filterByDepth(ArrayList<QuakeEntry> quakeData, double minDepth, double maxDepth) {      
         ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
         // TODO
+        System.out.println("Find quakes with depth between " + minDepth + " and " + maxDepth + "\n");
         for (QuakeEntry qe : quakeData) {
             if (qe.getDepth() > minDepth && qe.getDepth() < maxDepth) {
                 answer.add(qe);
@@ -37,9 +38,7 @@ public class EarthQuakeClient {
         return answer;
     }
     
-    public void quakesOfDepth(){
-        
-    }
+    
             
     public void dumpCSV(ArrayList<QuakeEntry> list){
         System.out.println("Latitude,Longitude,Magnitude,Info");
@@ -63,8 +62,9 @@ public class EarthQuakeClient {
         for (QuakeEntry qe : listBig) {
            System.out.println(qe); 
         }
+        System.out.println("\nFound "+listBig.size()+" quakes that match that criteria");
     }
-	
+    
     public void createCSV(){
         EarthQuakeParser parser = new EarthQuakeParser();
         String source = "data/nov20quakedata.atom";
@@ -81,9 +81,6 @@ public class EarthQuakeClient {
         ArrayList<QuakeEntry> list = parser.read(source);
         System.out.println("# quakes read: " + list.size());
         
-        //Durham, NC
-        //Location city = new Location(35.988, -78.907);
-        //Bridgeport, CA
         Location city = new Location(38.17, -118.82);
         ArrayList<QuakeEntry> close = filterByDistanceFrom(list, 1000*1000, city);
         for (int k=0; k< close.size(); k++) {
@@ -91,6 +88,20 @@ public class EarthQuakeClient {
             double distanceInMeters = city.distanceTo(entry.getLocation());
             System.out.println(distanceInMeters/1000 + " " + entry.getInfo());
         }
-
+        System.out.println("\nFound "+close.size()+" quakes that match that criteria");
+    }
+    
+    public void quakesOfDepth(){
+        EarthQuakeParser parser = new EarthQuakeParser();
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedata.atom";
+        ArrayList<QuakeEntry> list = parser.read(source);
+        System.out.println("read data for " + list.size() + " quakes");
+        
+        ArrayList<QuakeEntry> listDepth = filterByDepth(list, -10000.0, -5000.0);
+        for (QuakeEntry qe : listDepth) {
+           System.out.println(qe); 
+        }
+        System.out.println("\nFound "+listDepth.size()+" quakes that match that criteria");
     }
 }
