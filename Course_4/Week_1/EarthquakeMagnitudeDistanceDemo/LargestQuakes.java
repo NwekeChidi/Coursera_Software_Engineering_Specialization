7ef544fbf388ffe09015bed621dd46ba2a273891
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-11-16 08:11:22 +0100

    Added method 'getLargest'

--- Course_4/Week_1/EarthquakeMagnitudeDistanceDemo/LargestQuakes.java
+++ Course_4/Week_1/EarthquakeMagnitudeDistanceDemo/LargestQuakes.java
@@ -20,9 +20,14 @@ public class LargestQuakes {
         }
         */
         System.out.println("Read data for " + list.size() + " quakes");
-        int idxMaxDepth = indexOfLargest(list);
+        int idxMaxDepth = indexOfLargest(list), howMany = 5;
         System.out.println("Index of earthquake with max depth: "+idxMaxDepth+ ", with magnitude: "
                             +list.get(idxMaxDepth).getMagnitude());
+        System.out.println("\nThe first "+howMany+" largest quakes in file are:");
+        ArrayList<QuakeEntry> largest = getLargest(list, howMany);
+        for(QuakeEntry qe : largest){
+            System.out.println(qe);
+        }
     }
     
     public int indexOfLargest(ArrayList<QuakeEntry> data){
@@ -36,5 +41,15 @@ public class LargestQuakes {
             runIdx++;
         }
         return idx;
+    }
+    
+    public ArrayList<QuakeEntry> getLargest(ArrayList<QuakeEntry> quakeData, int howMany){
+        ArrayList<QuakeEntry> largest = new ArrayList<QuakeEntry>();
+        ArrayList<QuakeEntry> copy = quakeData;
+        for (int i = 0; i < howMany; i++){
+            largest.add(copy.get(indexOfLargest(copy)));
+            copy.remove(copy.get(indexOfLargest(copy)));
+        }
+        return largest;
     }
 }
