717714f5ee22bcc6179d418ac51df46a6cacca01
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-11-20 09:13:09 +0100

    Added class 'MagnitudeFilter'

---
+++ Course_4/Week_1/EarthquakeFilterStarterProgram/MagnitudeFilter.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of MagnitudeFilter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MagnitudeFilter implements Filter {
+    
+    private double minMag, maxMag;
+    
+    public MagnitudeFilter(double min, double maxi){
+        minMag = min;
+        maxMag = maxi;
+    }
+    
+    public boolean satisfies(QuakeEntry qe){
+        return qe.getMagnitude() >= minMag && qe.getMagnitude() <= maxMag;
+    }
+}
